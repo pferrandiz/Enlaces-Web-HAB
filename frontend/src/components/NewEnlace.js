a0ef4e6561a7922fe6e6e6094982902912f37954
pferrandiz <95432114+pferrandiz@users.noreply.github.com>
2022-06-08 19:42:39 +0200

    feat:

---
+++ frontend/src/components/NewEnlace.js
@@ -0,0 +1,24 @@
+export const NewEnlace = () => {
+  return (
+    <form>
+      <h1>Publica nuevo Enlace</h1>
+      <fieldset>
+        <label htmlFor="title">Título</label>
+        <input type="text" id="title" name="title" required />
+      </fieldset>
+      <fieldset>
+        <label htmlFor="url">Nuevo Enlace</label>
+        <input type="url" id="url" name="url" required />
+      </fieldset>
+      <fieldset>
+        <label htmlFor="text">Título</label>
+        <input type="text" id="text" name="text" />
+      </fieldset>
+      <fieldset>
+        <label htmlFor="image">Título</label>
+        <input type="file" id="image" name="image" />
+      </fieldset>
+      <button>Publicar Enlace</button>
+    </form>
+  );
+};
