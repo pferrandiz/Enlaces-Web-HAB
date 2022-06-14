07260f9167039bc59b7ae7f77040888a06ec26e2
pferrandiz <95432114+pferrandiz@users.noreply.github.com>
2022-06-14 17:30:10 +0200

    feat(onSubmit en New Enlaces):

--- frontend/src/components/NewEnlace.js
+++ frontend/src/components/NewEnlace.js
@@ -3,10 +3,13 @@ import { AuthContext } from "../context/AuthContext";
 import { sendEnlaceService } from "../comunicaciones";
 
 export const NewEnlace = ({ addEnlace }) => {
-  const [error, setError] = useState("");
   const [sending, setSending] = useState(false);
-  const [image, setImage] = useState();
   const { token } = useContext(AuthContext);
+  const [error, setError] = useState("");
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
+  const [text, setText] = useState("");
+  const [image, setImage] = useState();
 
   const handleForm = async (e) => {
     e.preventDefault();
@@ -15,7 +18,14 @@ export const NewEnlace = ({ addEnlace }) => {
       setSending(true);
 
       const data = new FormData(e.target);
-      const enlace = await sendEnlaceService({ data, token });
+      const enlace = await sendEnlaceService({
+        data,
+        token,
+        title,
+        url,
+        text,
+        image,
+      });
 
       addEnlace(enlace);
       e.target.reset();
@@ -28,42 +38,61 @@ export const NewEnlace = ({ addEnlace }) => {
   };
 
   return (
-    <form onSubmit={handleForm}>
+    <>
       <h1>Publica nuevo Enlace</h1>
-      <fieldset>
-        <label htmlFor="title">Título</label>
-        <input type="text" id="title" name="title" required />
-      </fieldset>
-      <fieldset>
-        <label htmlFor="url">Nuevo Enlace</label>
-        <input type="url" id="url" name="url" required />
-      </fieldset>
-      <fieldset>
-        <label htmlFor="text">Texto</label>
-        <input type="text" id="text" name="text" />
-      </fieldset>
-      <fieldset>
-        <label htmlFor="image">Imagen</label>
-        <input
-          type="file"
-          id="image"
-          name="image"
-          accept="image/*"
-          onChange={(e) => setImage(e.target.files[0])}
-        />
-        {image ? (
-          <figure>
-            <img
-              src={URL.createObjectURL(image)}
-              style={{ width: "200px" }}
-              alt="Preview"
-            />
-          </figure>
-        ) : null}
-      </fieldset>
-      <button>Publicar Enlace </button>
-      {sending ? <p>Enviando Enlace....</p> : null};
-      {error ? <p>{error}</p> : null}
-    </form>
+      <form onSubmit={handleForm}>
+        <fieldset>
+          <label htmlFor="title">Título</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            required
+            onChange={(e) => setTitle(e.target.files)}
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="url">Nuevo Enlace</label>
+          <input
+            type="url"
+            id="url"
+            name="url"
+            required
+            onChange={(e) => setUrl(e.target.files)}
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="text">Texto</label>
+          <input
+            type="text"
+            id="text"
+            name="text"
+            onChange={(e) => setText(e.target.files)}
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="image">Imagen</label>
+          <input
+            type="file"
+            id="image"
+            name="image"
+            accept="image/*"
+            onChange={(e) => setImage(e.target.files[0])}
+          />
+          {image ? (
+            <figure>
+              <img
+                src={URL.createObjectURL(image)}
+                style={{ width: "200px" }}
+                alt="Preview"
+              />
+            </figure>
+          ) : null}
+        </fieldset>
+        <button>Publicar Enlace </button>
+        {sending ? <p>Enviando Enlace....</p> : null};
+        {error ? <p>{error}</p> : null}
+      </form>
+    </>
   );
 };
