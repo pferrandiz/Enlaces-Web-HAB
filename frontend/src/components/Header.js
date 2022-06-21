bd2b6d709f6390e34b2f2968913b216ce7e584a3
pferrandiz <95432114+pferrandiz@users.noreply.github.com>
2022-06-21 19:02:05 +0200

    Css Header y Footer

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,14 @@
 import { Auth } from "./Auth";
 import { Link } from "react-router-dom";
+import "./Header.css";
 
 export const Header = () => {
   return (
     <header>
-      <h1>
+      <h1 className="link">
         <Link to={"/home"}>tuEnlace</Link>
       </h1>
+
       <nav>
         <Auth />
       </nav>
